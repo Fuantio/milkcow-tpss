79208df7785f6fbcc570b236650169d034d13e87
Fuan Bonilla <juan-guzman_bonilla@hotmail.com>
2022-08-27 14:28:01 -0500

    welcome.blade.php

---
+++ welcome.blade.php
@@ -0,0 +1,35 @@
+[33mcommit aa7ee2f32f332b74209670a47b49aaa42176d40f[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Fuan Bonilla <juan-guzman_bonilla@hotmail.com>
+Date:   Sat Aug 27 14:24:10 2022 -0500
+
+    Welcome.blade.php
+
+[33mcommit 5df5fbbf0e3e4fe1d1f317c00df4cd32f262bb46[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/frontend[m[33m)[m
+Author: JhormanSG <jhormansaavedra2407@gmail.com>
+Date:   Sat Aug 27 10:02:15 2022 -0500
+
+    Modificaciones en home.blade, app.blade
+
+[33mcommit bc888253787a758520198de7c570eba6cd89a6e3[m
+Author: JhormanSG <jhormansaavedra2407@gmail.com>
+Date:   Fri Aug 26 13:21:51 2022 -0500
+
+    Modificacion de estilos en app.blade, agregarNovedadAnimal, novedadesAnimal
+
+[33mcommit 94d34a75366966dc600f73b2472b37e6437abfc2[m
+Author: JhormanSG <jhormansaavedra2407@gmail.com>
+Date:   Fri Aug 26 08:33:27 2022 -0500
+
+    Cambio de botones de editar/eliminar
+
+[33mcommit b991c366032392b9d4f0fa72cc7655b38f37aa52[m[33m ([m[1;31morigin/novedad_p[m[33m, [m[1;32mnovedad_p[m[33m, [m[1;32mfrontend[m[33m)[m
+Author: Fuan Bonilla <juan-guzman_bonilla@hotmail.com>
+Date:   Sun Aug 21 21:50:38 2022 -0500
+
+    archivos faltantes xd
+
+[33mcommit 32d1a7835ba68615709dc67d6f4bc7c41b652887[m[33m ([m[1;31morigin/vacas[m[33m, [m[1;31morigin/usuarios[m[33m, [m[1;31morigin/despacho[m[33m, [m[1;32mvacas[m[33m, [m[1;32musuarios[m[33m, [m[1;32mdespacho[m[33m)[m
+Author: Fuan Bonilla <juan-guzman_bonilla@hotmail.com>
+Date:   Sun Aug 21 21:37:31 2022 -0500
+
+    inicio con git - mi primer commit
